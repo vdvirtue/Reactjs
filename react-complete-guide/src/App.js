12dd4326f4e549c738dfbedfcf0b3bfd86cad33d
vdvirtue <buivanducbk@gmail.com>
2019-07-01 11:46:45 +0700

    first commit react complete guide

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Xin chao! toi la mot ung dung react</h1>
+    </div>
+  );
+}
+
+export default App;
